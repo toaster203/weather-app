2240ef936ad3ee3df6f91d1bd1330a4c7a20c91f
Nicole Cui <nicolexcui@gmail.com>
2023-12-30 19:49:47 -0500

    Update Weather.tsx

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -22,12 +22,16 @@ export default function WeatherBox({ long, lat }) {
         getWeather();
     }, [long, lat]);
 
+    const search = async () => {
+        // idk if this is supposed to be here
+    }
+
     if (loaded) {
         return (
             <div className="weather">
                 <div className="search-bar">
                     <input type="text" className="cityInput" placeholder="Search"/>
-                    <div className="search-icon">
+                    <div className="search-icon" onClick={()=>{search()}}>
                         <img src={search_icon} alt="" />
                     </div>
                 </div>
